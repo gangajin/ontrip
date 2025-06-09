487474694c31295c3e8e021df8345c14f5e5ffe1
rlaek <rlaek@DESKTOP-SQTP90F>
2025-06-09 14:26:51 +0900

    2025/06/09 장소삽입(관리자) 박상준

--- src/main/java/com/onTrip/dao/PlaceDao.java
+++ src/main/java/com/onTrip/dao/PlaceDao.java
@@ -1,5 +1,18 @@
 package com.onTrip.dao;
 
-public interface PlaceDao {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.onTrip.dto.PlaceDto;
+@Mapper
+public interface PlaceDao {
+	
+	public List<PlaceDto> placeHotelList(int destinationNum);
+	
+    List<PlaceDto> searchHotel(@Param("destinationNum") int destinationNum,
+    							@Param("keyword") String keyword);
+	
+	public int insertPlace(PlaceDto Placedto);
 }
