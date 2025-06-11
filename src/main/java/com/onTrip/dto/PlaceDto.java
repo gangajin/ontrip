12900b1e4741ecf6bcc5ac6297f6d4ec671e041d
Ryu <jhlyu0605@naver.com>
2025-06-11 10:13:10 +0900

    충돌 해결 방안 - PlaceDto 수정 반영

--- src/main/java/com/onTrip/dto/PlaceDto.java
+++ src/main/java/com/onTrip/dto/PlaceDto.java
@@ -1,5 +1,21 @@
 package com.onTrip.dto;
 
-public class PlaceDto {
+import lombok.Data;
 
+@Data
+public class PlaceDto {
+    private int placeNum;
+    private String placeName ;
+    private String placeCategory; 
+    private String placeRoadAddr ;
+    private String placeDetailAddr;
+    private double placeLat;
+    private double placeLong;
+    private String placeImage;
+    private String kakaoPlaceId;
+    private String placeContent;
+    private int destinationNum;
+    private String placeExternalApiId;
+    private int placelike;
+    private double placeScore; 
 }
