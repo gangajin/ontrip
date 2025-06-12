e6d9b405f558a420d8258409de28e3d58c5f4064
lg <lg@DESKTOP-6KTSV4L>
2025-06-12 17:45:12 +0900

    가보자

--- src/main/java/com/onTrip/dao/ScheduleDao.java
+++ src/main/java/com/onTrip/dao/ScheduleDao.java
@@ -1,5 +1,21 @@
 package com.onTrip.dao;
 
-public interface ScheduleDao {
+import java.time.LocalDate;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.onTrip.dto.ScheduleDto;
+@Mapper
+public interface ScheduleDao {
+	// Schedule insert
+    void insertSchedule(@Param("destinationNum") int destinationNum,
+                        @Param("scheduleStart") LocalDate scheduleStart,
+                        @Param("scheduleEnd") LocalDate scheduleEnd,
+                        @Param("userNum") int userNum);
+
+    // 방금 insert된 Schedule PK 값 가져오기
+    int getLastInsertId();
+    
+    ScheduleDto getScheduleByNum(int scheduleNum);
 }
